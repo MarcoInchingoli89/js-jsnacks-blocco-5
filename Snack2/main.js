c38d04c9ac08e301332251e67ca8e8d1fb5dc030
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-24 17:03:29 +0200

    Initial Commit Snack2

--- Snack2/main.js
+++ Snack2/main.js
@@ -1,78 +1,68 @@
-/* Crea un array di 10 oggetti che rappresentano una zucchina, indicando
-per ognuna varietà, peso e lunghezza.
-Calcola quanto pesano tutte le zucchine. */
+/* Crea 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+Infine stampa separatamente quanto pesano i due gruppi di zucchine. */
 
 //Creo un array di zucchine con varietà, peso e lunghezza
 const zucchine = [
     {
         variety: 'Zucchina nera',
         weight: 100,
-        length: '10cm',
+        length: 10,
     },
 
     {
         variety: 'Zucchina romanesca',
         weight: 50,
-        length: '10cm',
+        length: 15,
     },
 
     {
         variety: 'Zucchina di Faenza',
         weight: 150,
-        length: '10cm',
+        length: 30,
     },
 
     {
         variety: 'Zucchina lunga fiorentina',
         weight: 200,
-        length: '10cm',
+        length: 25,
     },
 
     {
         variety: 'Zucchina siciliana',
         weight: 100,
-        length: '10cm',
+        length: 8,
     },
 
     {
         variety: 'Zucchina striata di Napoli',
         weight: 80,
-        length: '10cm',
+        length: 13,
     },
 
     {
         variety: 'Zucchina bianca triestina',
         weight: 75,
-        length: '10cm',
+        length: 16,
     },
 
     {
         variety: 'Zucchina rigata pugliese',
         weight: 120,
-        length: '10cm',
+        length: 20,
     },
 
     {
         variety: 'Zucchina tonda di Piacenza',
         weight: 60,
-        length: '10cm',
+        length: 10,
     },
 
     {
         variety: 'Zucchina tonda di Nizza',
         weight: 80,
-        length: '10cm',
+        length: 11,
     }
 ];
 
-//Vado a prendere il peso delle zucchine e lo vado a sommare
-let sum = 0;
-for (let i = 0; i < zucchine.length; i++) {
-    const zucchina = zucchine[i];
-    /* console.log(zucchina); */
-    const pesoZucchina = zucchina.weight;
-    /* console.log(pesoZucchina); */
-    sum += pesoZucchina;
-}
-
-console.log(sum);
+//Dividi in due array le zucchine che sono lunghe più e meno di 15cm
