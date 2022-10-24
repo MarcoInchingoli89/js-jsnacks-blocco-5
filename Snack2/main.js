71cd604731c0e7e7f12301b96cad0f008b94fa95
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-24 17:53:34 +0200

    Add Snack2 Completed

--- Snack2/main.js
+++ Snack2/main.js
@@ -66,3 +66,32 @@ const zucchine = [
 ];
 
 //Dividi in due array le zucchine che sono lunghe più e meno di 15cm
+
+//Array zucchine lunghe più di 15cm
+const zucchineLunghe = zucchine.filter(zucchina => zucchina.length > 15);
+console.log(zucchineLunghe);
+//Array zucchine lunghe meno di 15cm
+const zucchineCorte = zucchine.filter(zucchina => zucchina.length <= 15);
+console.log(zucchineCorte);
+//Peso zucchine lunghe
+let sumLong = 0;
+for (let i = 0; i < zucchineLunghe.length; i++) {
+    const zucchinaLunga = zucchineLunghe[i];
+    /* console.log(zucchina); */
+    const pesoZucchinaLunga = zucchinaLunga.weight;
+    /* console.log(pesoZucchina); */
+    sumLong += pesoZucchinaLunga;
+}
+
+console.log(sumLong);
+//Peso zucchine corte
+let sumShort = 0;
+for (let i = 0; i < zucchineCorte.length; i++) {
+    const zucchinaCorta = zucchineCorte[i];
+    /* console.log(zucchina); */
+    const pesoZucchinaCorta = zucchinaCorta.weight;
+    /* console.log(pesoZucchina); */
+    sumShort += pesoZucchinaCorta;
+}
+
+console.log(sumShort);
